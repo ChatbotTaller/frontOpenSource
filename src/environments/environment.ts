898ddc0f425b41f2826fc5a8d4c74aa059cbc047
Miguel Angel Escobar Gomez <maeg2468@gmail.com>
2026-07-17 01:56:45 -0500

    arreglos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://backendopensource-production.up.railway.app/',
+    apiUrl: 'https://backendopensource-production.up.railway.app',
 };
